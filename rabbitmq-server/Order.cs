4581175bf9080cad3e2028cfa4ddb9ab5466c25b
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2023-12-10 16:07:17 +0100

    created models
    Some basic models have been created for the messaging

---
+++ rabbitmq-server/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace rabbitmq_server
+{
+    public class Order
+    {
+        public string Id { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public string ProductId { get; set; } = null!;
+        public int Quantity { get; set; }
+
+        public override string ToString() => $"Order Id: {Id}\nUserId: {UserId}\nProductId: {ProductId}\nQuantity: {Quantity}";
+    }
+}
---
+++ rabbitmq-server/Report.cs
@@ -0,0 +1,24 @@
+﻿namespace rabbitmq_server
+{
+    public class Report
+    {
+        public class Item
+        {
+            public string ProductId { get; set; } = null!;
+            public int Quantity { get; set; }
+        }
+
+        public List<Item> Items { get; set; } = null!;
+
+        public override string ToString()
+        {
+            if (Items.Count == 0)
+                return "No item";
+
+            string output = "";
+            Items.ForEach(_ => output += $"{_.ProductId} was sold {_.Quantity} times\n");
+
+            return output;
+        }
+    }
+}
