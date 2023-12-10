61fb9ad10b32905bfeda71297bed246d27fe96ad
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2023-12-10 16:32:39 +0100

    added topic exchange messaging
    The server sends messages about the products (sold and refund), and the stock application listens to the related queue in order to receive the messages

---
+++ rabbitmq-server/Server.cs
@@ -0,0 +1,81 @@
+﻿using RabbitMQ.Client;
+using rabbitmq_server;
+using System.Text;
+using System.Text.Json;
+
+//~ Begin - Connection to RabbitMQ
+ConnectionFactory factory = new ConnectionFactory()
+{
+    //Uri = new Uri("server url")
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+IConnection connection = factory.CreateConnection();
+//~ End
+
+
+//~ Begin - Messaging methods
+void OnSoldProduct(Order order)
+{
+    using (IModel channel = connection.CreateModel())
+    {
+        channel.ExchangeDeclare("Product", ExchangeType.Topic, true);
+
+        /**
+         * Uncomment it in order to receive the messages that are sent before any consumer is created
+         */
+        //channel.QueueDeclare("ProductQueue", true, false, false);
+        //channel.QueueBind("ProductQueue", "Product", "product.sold");
+
+        string json = JsonSerializer.Serialize(order);
+        byte[] package = Encoding.UTF8.GetBytes(json);
+
+        IBasicProperties properties = channel.CreateBasicProperties();
+        properties.Persistent = true;
+
+        channel.BasicPublish("Product", "product.sold", properties, package);
+
+        Console.WriteLine("The sold product message has been sent");
+    }
+}
+
+void OnRefundedProduct(string orderId)
+{
+    using (IModel channel = connection.CreateModel())
+    {
+        channel.ExchangeDeclare("Product", ExchangeType.Topic, true);
+
+        /**
+         * Uncomment it in order to receive the messages that are sent before any consumer is created
+         */
+        //channel.QueueDeclare("ProductQueue", true, false, false);
+        //channel.QueueBind("ProductQueue", "Product", "product.refund");
+
+        byte[] package = Encoding.UTF8.GetBytes(orderId);
+
+        IBasicProperties properties = channel.CreateBasicProperties();
+        properties.Persistent = true;
+
+        channel.BasicPublish("Product", "product.refund", properties, package);
+
+        Console.WriteLine("The refunded product message has been sent");
+    }
+}
+//~ End
+
+
+// ... send the messages
+OnSoldProduct(new Order()
+{
+    Id = Guid.NewGuid().ToString(),
+    UserId = Guid.NewGuid().ToString(),
+    ProductId = Guid.NewGuid().ToString(),
+    Quantity = new Random().Next(1, 11)
+});
+
+OnRefundedProduct(Guid.NewGuid().ToString());
+
+Console.ReadKey();
---
+++ rabbitmq-stock/StockConsole.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using rabbitmq_server;
+using System.Text;
+using System.Text.Json;
+
+//~ Begin - Connection to RabbitMQ
+ConnectionFactory factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672
+};
+
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+//~ End
+
+channel.ExchangeDeclare("Product", ExchangeType.Topic, true);
+
+/**
+ * Give a static queue name in order to read the messages before this consumer is created
+ * In this case the name is 'ProductQueue'. Also remove the Bind method
+ */
+string queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queueName, "Product", "product.*");
+channel.BasicQos(0, 1, false);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queueName, false, consumer);
+consumer.Received += (sender, e) =>
+{
+    if (e.RoutingKey == "product.sold")
+    {
+        string json = Encoding.UTF8.GetString(e.Body.ToArray());
+        Order? order = JsonSerializer.Deserialize<Order>(json);
+
+        Console.WriteLine(order?.ToString());
+    }
+    else if (e.RoutingKey == "product.refund")
+    {
+        string orderId = Encoding.UTF8.GetString(e.Body.ToArray());
+        Console.WriteLine($"{orderId} was refunded");
+    }
+    else return;
+
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+Console.ReadKey();
